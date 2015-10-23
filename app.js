02eca8dced90b2c61f44f219df7886e9351defac
Mg <gamem.8818@gmail.com>
2015-10-23 13:58:33 -0700

    add index

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('combined'));
 
+app.get('/', function(req, res) {
+    	res.render('index.html')
+});
+
 app.get('/getRequest1',function(req, res){
 	console.log("get request1")
-	res.send('get request1 from ' + req.headers.host)
+	res.send('get request1 from ' + req.rawHeaders)
 });
 
 app.get('/getRequest2',function(req, res){
 	console.log("get request2")
-	res.send('get request2 from ' + req.headers.host)
+	res.send('get request2 from ' + req.rawHeaders)
+});
+
+app.get('/getRequestWin',function(req, res){
+	console.log("Windows get request")
+	res.send('Windows get request: ' + req.rawHeaders)
 });
 
 var httpListener = http.createServer(app);
