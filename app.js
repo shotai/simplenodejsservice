a8fabd8b38c58274f0ede4db5fb91abbed8fe01e
Mg <gamem.8818@gmail.com>
2015-10-26 07:41:02 -0700

    update appjs

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ app.use(function(req, res, next) {
   next();
 });
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    next();
+}
+
+//app.use(allowCrossDomain);
 
 
 app.get('/', function(req, res, next) {
